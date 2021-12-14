8f1f31c93eb61f32f4e5281e37926e46ce754630
learningcpp834 <95869708+learningcpp834@users.noreply.github.com>
2021-12-14 10:56:52 -0500

    Update books.js

--- js/books.js
+++ js/books.js
@@ -2,4 +2,5 @@ function show(){
   var reader = document.getElementById("reader")
   reader.style.display ="block";
   reader.innerText = document.getElementById("book").innerText;
+  localStorage.setItem("story1", reader.innerText);
 }
