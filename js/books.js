8772f0738be8aea25b4f0d360d7af406aa77d1a4
writebooksonline <89612530+codingtutorials23@users.noreply.github.com>
2021-12-14 15:27:16 -0500

    Delete books.js

--- js/books.js
+++
@@ -1,6 +0,0 @@
-function show(){
-  var reader = document.getElementById("reader")
-  reader.style.display ="block";
-  reader.innerText = document.getElementById("book").innerText;
-  localStorage.setItem("story1", reader.innerText);
-}
